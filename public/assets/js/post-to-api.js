ea776899274d4f648b4f05fd4e104fa05ff67a34
robiky <itakpoodiso@gmail.com>
2020-03-29 20:10:46 +0400

    first commit

---
+++ public/assets/js/post-to-api.js
@@ -0,0 +1,76 @@
+
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+//
+//// Update the current slider value (each time you drag the slider handle)
+//slider.oninput = function() {
+//  output.innerHTML = this.value;
+//}
+
+function postToGoogle() {
+
+
+
+
+    var field1 = $("#fname").val();
+    var field2 = $("#lname").val();
+    var field3 = $("#inputEmail").val();
+    var field4 = $("#phone").val();
+    var field5 = $("#description").val().trim();
+     var field6 = $("#inputWho").val();
+     var field7 = $("#inputWhat").val();
+     var field8 = $("#inputWhere").val();
+     var field9 = $("#inputLength").val();
+       var content = " I am " + field6 + " Looking for " + field7 + " at  " + field8 + " for "  + field9 + " moredetails " + field5
+
+    /*if(field1 == ""){
+        alert('Please Fill Your Name');
+        document.getElementById("contact_name").focus();
+
+    }
+    if(field2 == ""){
+        alert('Please Fill Your Email');
+        document.getElementById("contact_email").focus();
+
+    }
+    if(field3 == "" || field3.length > 5 || field3.length < 10){
+        alert('Please Fill Your Mobile Number');
+        document.getElementById("contact_phone").focus();
+    }*/
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://crm.housemasters.mu/espocrm/api/v1/LeadCapture/e9523b855a02c0382120e21f4416f498",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+
+        },
+
+
+        "data": "{\"firstName\": \""+field1+"\",\"lastName\": \""+field2+"\",\"emailAddress\": \""+field3+"\",\"phoneNumber\": \""+field4+"\",\"description\": \""+content+"\",\"phoneNumberIsOptedOut\":1}"
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(field5);
+        if(response == true){
+            $('#requestForm').hide();
+            $('#Submit').hide();
+            $('#note').hide();
+            $('.alert').show();
+
+        }
+        if(response != true){
+            //$('#requestForm').hide();
+            //$('#Submit').hide();
+            //$('#note').hide();
+           // $('.alert').show();
+
+        }
+    });
+
+    return false;
+}
---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+require('./bootstrap');
+require('./custom-script');
+require('./post-to-api');
+require('./script.min');
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        xfbml            : true,
+        version          : 'v3.3'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
